3f6f375569be72c6596f03d0b464e2a277e4bad9
AlehZakharau <Alehzaharau@tutanota.com>
2022-03-22 12:04:36 -0400

    input

---
+++ Assets/Code/GameLifeTimeScope.cs
@@ -0,0 +1,15 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Code
+{
+    public class GameLifeTimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<IPlayerInput, PlayerInput>(Lifetime.Singleton);
+            
+            base.Configure(builder);
+        }
+    }
+}
---
+++ Assets/Code/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEditor;
+
+namespace Code
+{
+    public interface IPlayerInput
+    {
+        public Actions Actions { get; }
+        public void Disable();
+        public void Enable();
+    }
+    public class PlayerInput : IPlayerInput, IDisposable
+    {
+        private readonly Actions actions;
+        public Actions Actions => actions;
+
+        public PlayerInput()
+        {
+            actions = new Actions();
+            actions.Enable();
+        }
+        
+        public void Disable()
+        {
+            actions.Disable();
+        }
+
+        public void Enable()
+        {
+            actions.Enable();
+        }
+
+        public void Dispose()
+        {
+            actions?.Dispose();
+        }
+    }
+}
